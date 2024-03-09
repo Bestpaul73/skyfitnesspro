c5ee28cd5e5b42b961dba2e0d669a2a1ccc98afd
Paul Palkin <111692145+Bestpaul73@users.noreply.github.com>
2024-03-09 12:01:47 +0300

    rtp

--- src/pages/progress/Progress.jsx
+++ src/pages/progress/Progress.jsx
@@ -5,16 +5,19 @@ import { useSelector } from 'react-redux';
 // import { getCurrentUsers } from '../api';
 import { hidePopupFlag } from '../../components/hidePopup/hidePopupFlag';
 import { useNavigate } from 'react-router-dom';
+import Hand from './Hand.png';
+import { ProgressCheck } from './ProgressCheck'
 
 export const Progress = () => {
   const navigate = useNavigate();
 
-  const workouts = useSelector((state) => state.coursesApp.allWorkouts);
+
+  // const workouts = useSelector((state) => state.coursesApp.allWorkouts);
   const currentWorkout = useSelector((state) => state.coursesApp.currentWorkout);
   // const workout = workouts?.filter((data) => data._id.includes(currentWorkout));
 
-  console.log(workouts);
-  console.log(currentWorkout);
+  // console.log(workouts);
+  // console.log(currentWorkout);
   // console.log(workout);
   // console.log(workout[0].exercises);
   // console.log(currentWorkout.exercises);
@@ -45,36 +48,74 @@ export const Progress = () => {
 
   useEffect(() => onFocusFirstInput(), []);
 
+  let progressCheckFlag = true;
+  console.log(progressCheckFlag);
+  
+  const handleSubmit = () => {
+    progressCheckFlag = !progressCheckFlag;
+    console.log(progressCheckFlag);
+  };
+
+  // useEffect(() => handleSubmit(), [handleSubmit()])
+
   return (
-    <div
-      className={styles.popup_wrapper}
-      onMouseUp={(e) => hidePopup(e, 'mouse')}
-      onKeyDown={(e) => hidePopup(e, 'kbd')}
-    >
-      <div className={styles.progressForm} id='#popup'>
-        <div className={styles.headerForm}>Мой прогресс</div>
-        {currentWorkout?.exercises.map((el, index) => {
-          return (
-            <div key={index}>
-              <div className={styles.textForm}>
-                Сколько раз вы сделали: <br />"{el.name}"?
-              </div>
-              <div className={styles.inputBox}>
-                <input
-                  className={styles.inputForm}
-                  type='number'
-                  name='quantity'
-                  placeholder='Введите значение'
-                  onChange={(e) => handleInputChange(e, el.name)}
-                ></input>
-              </div>
+    <>
+      {progressCheckFlag ? (
+        // <div className={styles.page}>
+        //   <div className={styles.progressCheckForm}>
+        //     <div className={styles.headerProgress}>Ваш прогресс засчитан!</div>
+        //     <div className={styles.imgBox}>
+        //       <img className={styles.Hand} src={Hand} alt='Hand' />
+        //     </div>
+        //   </div>
+        // </div>
+
+        // <div
+        //   className={styles.popup_wrapper}
+        //   onMouseUp={(e) => hidePopup(e, 'mouse')}
+        //   onKeyDown={(e) => hidePopup(e, 'kbd')}
+        // >
+        //   <div className={styles.progressForm} id='#popup'>
+        //     <div className={styles.headerForm}>ААААААААААААААА</div>
+        //     <div className={styles.buttonBox}>
+        //       <Button className={'button_blue'} children={'Отправить'} onClick={handleSubmit} />
+        //     </div>
+        //   </div>
+        // </div>
+
+        <Progress/>
+      ) : (
+        <div
+          className={styles.popup_wrapper}
+          onMouseUp={(e) => hidePopup(e, 'mouse')}
+          onKeyDown={(e) => hidePopup(e, 'kbd')}
+        >
+          <div className={styles.progressForm} id='#popup'>
+            <div className={styles.headerForm}>Мой прогресс</div>
+            {currentWorkout?.exercises.map((el, index) => {
+              return (
+                <div key={index}>
+                  <div className={styles.textForm}>
+                    Сколько раз вы сделали: <br />"{el.name}"?
+                  </div>
+                  <div className={styles.inputBox}>
+                    <input
+                      className={styles.inputForm}
+                      type='number'
+                      name='quantity'
+                      placeholder='Введите значение'
+                      onChange={(e) => handleInputChange(e, el.name)}
+                    ></input>
+                  </div>
+                </div>
+              );
+            })}
+            <div className={styles.buttonBox}>
+              <Button className={'button_blue'} children={'Отправить'} onClick={handleSubmit} />
             </div>
-          );
-        })}
-        <div className={styles.buttonBox}>
-          <Button className={'button_blue'} children={'Отправить'} />
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
